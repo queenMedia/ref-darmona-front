cc718f00ad15b620f4e0cf7601637fbf762977e2
ronyderra <derrarony@gmail.com>
2023-08-14 17:48:16 +0300

    cookie

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -20,6 +20,7 @@ const Login = (props) => {
       localStorage.setItem("username", username);
       localStorage.setItem("password", password);
       const cookie = getCookie("jwt");
+      console.log({cookie});
       user.data.token = cookie;
       dispatch(setUser(user.data));
       navigate("/cmplist");
