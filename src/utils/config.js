a317e082c7c9b3e8a025aff410b972213a2449d8
ronyderra <derrarony@gmail.com>
2023-08-14 17:19:05 +0300

    baseUrl

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://localhost:3030"
-export const baseUrl = "https://darmona-aff3d37b387b.herokuapp.com"
+// export const baseUrl = "https://darmona-aff3d37b387b.herokuapp.com"
+export const baseUrl = "http://refapi.darmona.org/"
