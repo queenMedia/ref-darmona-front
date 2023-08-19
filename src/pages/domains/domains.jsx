caceb43fd1b44fad06557a58a083d17fe3d78bdb
ronyderra <derrarony@gmail.com>
2023-08-19 20:56:33 +0300

    Injector

--- src/pages/domains/domains.jsx
+++ src/pages/domains/domains.jsx
@@ -77,102 +77,108 @@ const Domains = () => {
   }, []);
 
   return (
-    <div className="snowPage-container">
-      <form className="snowPage-form" onSubmit={handleBpSubmit}>
-        <h2 className="form-title">Black Pages</h2>
-        <div className="form-body">
-          <select
-            className="form-select"
-            onChange={(e) => handleSelectGeo(e.target.value)}
-          >
-            <option value={"Asdf"}>Select Geo</option>
-            {geos.map((i, index) => {
-              return (
-                <option key={index} value={i}>
-                  {i.split("/")[0]}
-                </option>
-              );
-            })}
-          </select>
-          <select
-            className="form-select"
-            onChange={(e) => setLang(e.target.value)}
-          >
-            <option value={selectedLang}>All Languages</option>
+    <div className="domain-cont">
+      <div className="snowPage-container">
+        <form className="snowPage-form" onSubmit={handleBpSubmit}>
+          <h2 className="form-title">Black Pages</h2>
+          <div className="form-body">
+            <select
+              className="form-select"
+              onChange={(e) => handleSelectGeo(e.target.value)}
+            >
+              <option value={"Asdf"}>Select Geo</option>
+              {geos.map((i, index) => {
+                return (
+                  <option key={index} value={i}>
+                    {i.split("/")[0]}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              className="form-select"
+              onChange={(e) => setLang(e.target.value)}
+            >
+              <option value={selectedLang}>All Languages</option>
 
-            {languages.map((i, index) => {
-              return (
-                <option key={index} value={i}>
-                  {i.split("/")[1]}
-                </option>
-              );
-            })}
-          </select>
-          <button type="submit">Submit</button>
-          <select
-            className="form-select"
-            onChange={(e) => setDefCharacter(e.target.value)}
-          >
-            <option value={"Asdf"}>Available Charachters</option>
-            {characters.map((i, index) => {
-              return (
-                <option
-                  key={index}
-                  value={i.split("/")[2].replace(".json", " ")}
-                >
-                  {i.split("/")[2].replace(".json", " ")}
-                </option>
-              );
-            })}
-          </select>
-          <select
-            className="form-select"
-            onChange={(e) => setDefOffer(e.target.value)}
-          >
-            <option value={"Asdf"}>Available Offers</option>
-            {offers?.map((i, index) => {
-              if (notRelavantOffers.includes(i.split("/")[1])) {
-                return null;
-              }
-              return (
-                <option key={index} value={i.split("/")[1]}>
-                  {i.split("/")[1]}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-      </form>
-
-      <table className="snowPage-table">
-        {tableData?.length > 0 ? (
-          <>
-            <thead>
-              <tr>
-                <th>Link</th>
-                <th>Geo</th>
-                <th>Language</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableData?.map((item, index) => (
-                <tr key={index}>
-                  <td
-                    className="snowPage-link"
-                    onClick={() => handleCopy(handleLink(item))}
+              {languages.map((i, index) => {
+                return (
+                  <option key={index} value={i}>
+                    {i.split("/")[1]}
+                  </option>
+                );
+              })}
+            </select>
+            <button type="submit">Submit</button>
+            <select
+              className="form-select"
+              onChange={(e) => setDefCharacter(e.target.value)}
+            >
+              <option value={"Asdf"}>Available Charachters</option>
+              {characters.map((i, index) => {
+                return (
+                  <option
+                    key={index}
+                    value={i.split("/")[2].replace(".json", " ")}
                   >
-                    {handleLink(item)}
-                  </td>
-                  <td>{selectedGeo.replace("/", " ")}</td>
-                  <td>{selectedLang.split("/")[1]}</td>
+                    {i.split("/")[2].replace(".json", " ")}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              className="form-select"
+              onChange={(e) => setDefOffer(e.target.value)}
+            >
+              <option value={"Asdf"}>Available Offers</option>
+              {offers?.map((i, index) => {
+                if (notRelavantOffers.includes(i.split("/")[1])) {
+                  return null;
+                }
+                return (
+                  <option key={index} value={i.split("/")[1]}>
+                    {i.split("/")[1]}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </form>
+
+        <table className="snowPage-table">
+          {tableData?.length > 0 ? (
+            <>
+              <thead>
+                <tr>
+                  <th>Link</th>
+                  <th>Geo</th>
+                  <th>Language</th>
                 </tr>
-              ))}
-            </tbody>{" "}
-          </>
-        ) : (
-          <></>
-        )}
-      </table>
+              </thead>
+              <tbody>
+                {tableData?.map((item, index) => (
+                  <tr key={index}>
+                    <td
+                      className="snowPage-link"
+                      onClick={() => handleCopy(handleLink(item))}
+                    >
+                      {handleLink(item)}
+                    </td>
+                    <td>{selectedGeo.replace("/", " ")}</td>
+                    <td>{selectedLang.split("/")[1]}</td>
+                  </tr>
+                ))}
+              </tbody>{" "}
+            </>
+          ) : (
+            <></>
+          )}
+        </table>
+      </div>
+      <div className="snowPage-container">
+        <h2 className="form-title">Injector</h2>
+        
+      </div>
     </div>
   );
 };
