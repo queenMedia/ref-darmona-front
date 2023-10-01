b9bd68e09ccebaf936a5455ef53ce6eeefd6481b
ronyderra <derrarony@gmail.com>
2023-10-01 17:55:33 +0300

    container

--- src/pages/cmpList/cmpList.jsx
+++ src/pages/cmpList/cmpList.jsx
@@ -7,9 +7,10 @@ import "./cmpList.css";
 const CmpList = () => {
   const allCmps = useSelector((state) => state.user.cmps);
   const [search, setSearch] = useState("");
-  const [cmps, setCmps] = useState(useSelector((state) => state.user.cmps));
+  const [filterStatus, setFilterStatus] = useState(true);
+  const [cmps, setCmps] = useState(useSelector((state) => state.user.cmps.filter(i => i.status !== "burned")));
   const navigate = useNavigate();
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
 
   const handleSearch = () => {
     if (search) {
@@ -24,12 +25,18 @@ const CmpList = () => {
     }
   };
 
+  const handleShowAllCmpa = () => {
+    setCmps(filterStatus ? allCmps : allCmps.filter(i => i.status !== "burned"))
+    setFilterStatus(!filterStatus)
+  }
+
   return (
     <div className="cmp-list-container">
       <div className="cmp-search-container">
         <input placeholder="Search" onChange={(e) => setSearch(e.target.value)} onKeyPress={(e) => e.key === 'Enter' && handleSearch()} />
         {/* <button onClick={handleSearch}>Search</button> */}
         <button onClick={() => navigate("/cmplist/addnew")}>Add Cmp</button>
+        <input type="checkBox" onClick={handleShowAllCmpa}></input>
       </div>
       <Table data={cmps} />
     </div>
