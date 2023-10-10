8c52226421aca78ae2c2124f34c105b992e9497f
ronyderra <derrarony@gmail.com>
2023-10-10 17:38:13 +0300

    params

--- src/assets/data/params.js
+++ src/assets/data/params.js
@@ -3,7 +3,7 @@ export const params = [
         platform: "Bing", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}"
     },
     {
-        platform: "Facebook", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}"
+        platform: "Facebook", params: "ad_id={{ad.id}}&adset_id={{adset.id}}&campaign_id={{campaign.id}}&ad_name={{ad.name}}&adset_name={{adset.name}}&campaign_name={{campaign.name}}&placement={{placement}}&site_source_name={{site_source_name}}"
     },
     {
         platform: "TikTok", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}"
