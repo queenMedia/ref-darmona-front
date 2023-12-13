1cc76b9db998455acd659447c618cd33f9469b0e
ronyderra <derrarony@gmail.com>
2023-12-13 13:37:26 -0600

    params

--- src/assets/data/params.js
+++ src/assets/data/params.js
@@ -1,20 +1,21 @@
 export const params = [
     {
-        platform: "Bing", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink:"&campaignid=530663956&targetid=kwd-84251358364749:loc-109&matchtype=e&network=o&device=c&adid=84250629534898&keyword=Le%20Quotidien&msclkid=fada6d09bca616b06bc209af240ccff0&querystring=le%20quotidien&feeditemid="
+        platform: "Bing", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink: "&campaignid=530663956&targetid=kwd-84251358364749:loc-109&matchtype=e&network=o&device=c&adid=84250629534898&keyword=Le%20Quotidien&msclkid=fada6d09bca616b06bc209af240ccff0&querystring=le%20quotidien&feeditemid="
     },
     {
-        platform: "Facebook", params: "ad_id={{ad.id}}&adset_id={{adset.id}}&campaign_id={{campaign.id}}&ad_name={{ad.name}}&adset_name={{adset.name}}&campaign_name={{campaign.name}}&placement={{placement}}&site_source_name={{site_source_name}}", testLink:""
+        platform: "Google", params: "aff_sub1={campaignid}&aff_sub2={adgroupid}&aff_sub3={feeditemid}&aff_sub4={extensionid}&aff_sub5={targetid}&aff_sub6={loc_interest_ms}&aff_sub7={loc_physical_ms}&aff_sub8={matchtype}&aff_sub9={network}&aff_sub10={device}&aff_sub11={devicemodel}&aff_sub12={gclid}&aff_sub13={creative}&aff_sub14={keyword}&aff_sub15={placement}&aff_sub16={target}", testLink: "?aff_sub1=20841808285&aff_sub2=154739695165&aff_sub3=&aff_sub4=&aff_sub5=&aff_sub6=&aff_sub7=9067607&aff_sub8=&aff_sub9=&aff_sub10=t&aff_sub11=huawei%2Bags-w09&aff_sub12=CjwKCAiA1MCrBhAoEiwAC2d64czDHRTP_QSKvLXBDvTnogzcSr5WTVCj4_WOZtnUAQLSWpbr01I0fhoCbDcQAvD_BwE&aff_sub13=683531362512&aff_sub14=&aff_sub15=&aff_sub16=&gclid=CjwKCAiA1MCrBhAoEiwAC2d64czDHRTP_QSKvLXBDvTnogzcSr5WTVCj4_WOZtnUAQLSWpbr01I0fhoCbDcQAvD_BwE"
     },
     {
-        platform: "TikTok", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink:""
+        platform: "Taboola", params: "aff_sub={site}&aff_sub2={site_id}&aff_sub3={site_domain}&aff_sub4={thumbnail}&aff_sub5={title}&aff_sub6={platform}&aff_sub7={campaign_id}&aff_sub8={campaign_name}&aff_sub9={campaign_item_id}&aff_sub10={cpc}&aff_sub11={click_id}", testLink: ""
     },
     {
-        platform: "Twitter", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink:""
+        platform: "Facebook", params: "ad_id={{ad.id}}&adset_id={{adset.id}}&campaign_id={{campaign.id}}&ad_name={{ad.name}}&adset_name={{adset.name}}&campaign_name={{campaign.name}}&placement={{placement}}&site_source_name={{site_source_name}}", testLink: ""
     },
     {
-        platform: "Google", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink:"?aff_sub1=20841808285&aff_sub2=154739695165&aff_sub3=&aff_sub4=&aff_sub5=&aff_sub6=&aff_sub7=9067607&aff_sub8=&aff_sub9=&aff_sub10=t&aff_sub11=huawei%2Bags-w09&aff_sub12=CjwKCAiA1MCrBhAoEiwAC2d64czDHRTP_QSKvLXBDvTnogzcSr5WTVCj4_WOZtnUAQLSWpbr01I0fhoCbDcQAvD_BwE&aff_sub13=683531362512&aff_sub14=&aff_sub15=&aff_sub16=&gclid=CjwKCAiA1MCrBhAoEiwAC2d64czDHRTP_QSKvLXBDvTnogzcSr5WTVCj4_WOZtnUAQLSWpbr01I0fhoCbDcQAvD_BwE"
+        platform: "TikTok", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink: ""
     },
     {
-        platform: "Taboola", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink:""
-    }
+        platform: "Twitter", params: "campaignid={CampaignId}&targetid={TargetId}&matchtype={MatchType}&network={Network}&device={Device}&adid={AdId}&keyword={keyword:default}&msclkid={msclkid}&querystring={QueryString}&feeditemid={feeditemid}", testLink: ""
+    },
+
 ]
