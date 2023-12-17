a783dfbadcabdce5d3ff50d733baf53d2d745106
ronyderra <derrarony@gmail.com>
2023-12-17 12:55:33 -0600

    Select

--- src/components/getSnowRows/countByDateAndParam.jsx
+++ src/components/getSnowRows/countByDateAndParam.jsx
@@ -7,6 +7,8 @@ import { notify_error, notify_success, notify_Info } from "../../utils/notify";
 import { Select } from "../../origins/select/select.jsx";
 import { snowColums } from "../../assets/data/snowColums";
 import { handleCopy } from "../../utils/notify";
+import { formatDate } from "../../utils/getDate.js"
+import { betweenDates } from "../../assets/data/snowColums.js"
 import "./getSnowRows.css";
 
 const CountByDateAndParam = () => {
@@ -16,6 +18,7 @@ const CountByDateAndParam = () => {
   const [by, setBy] = useState("");
   const [data, setData] = useState([]);
   const [sortBy, setSortBy] = useState("not_passed");
+  const [showCostumDate, setShowCostumDate] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,30 +51,86 @@ const CountByDateAndParam = () => {
     setBy(val);
   };
 
+  const handleDateSelect = (selectedItem) => {
+    let selectedDate;
+
+    switch (selectedItem) {
+      case "Today":
+        selectedDate = new Date();
+        setShowCostumDate(false)
+        break;
+      case "Last 2 Days":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 1));
+        setShowCostumDate(false)
+        break;
+      case "Last 3 Days":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 2));
+        setShowCostumDate(false)
+        break;
+      case "Last 4 Days":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 3));
+        setShowCostumDate(false)
+        break;
+      case "Last 5 Days":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 4));
+        setShowCostumDate(false)
+        break;
+      case "Last 6 Days":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 5));
+        setShowCostumDate(false)
+        break;
+      case "Last Week":
+        selectedDate = new Date(new Date().setDate(new Date().getDate() - 7));
+        setShowCostumDate(false)
+        break;
+      case "Custom Date":
+        setShowCostumDate(true)
+        break;
+      default:
+        selectedDate = new Date();
+        setShowCostumDate(false)
+        break;
+    }
+    setDateFrom(formatDate(selectedDate))
+    setDateTo(formatDate(new Date()))
+  }
+
   return (
     <div className="snowPage-container">
       <form onSubmit={handleSubmit}>
         <Box>
           <h1>Snow Rows</h1>
-          <label>
-            Date From:
-            <input
-              className="input-date"
-              type="date"
-              value={dateFrom}
-              onChange={(e) => setDateFrom(e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Date To:
-            <input
-              className="input-date"
-              type="date"
-              value={dateTo}
-              onChange={(e) => setDateTo(e.target.value)}
-            />
-          </label>
+          <Select
+            className="selectWidth"
+            required={true}
+            data={betweenDates}
+            title={"Select Date"}
+            func={handleDateSelect}
+          />
+          {showCostumDate &&
+            <div className="customDate-select">
+              <label>
+                Date From:
+                <input
+                  className="input-date"
+                  type="date"
+                  value={dateFrom}
+                  onChange={(e) => setDateFrom(e.target.value)}
+                  required={true}
+                />
+              </label>
+              <br />
+              <label>
+                Date To:
+                <input
+                  className="input-date"
+                  type="date"
+                  value={dateTo}
+                  onChange={(e) => setDateTo(e.target.value)}
+                  required={true}
+                />
+              </label>
+            </div>}
           <br />
           <Select
             className="selectWidth"
