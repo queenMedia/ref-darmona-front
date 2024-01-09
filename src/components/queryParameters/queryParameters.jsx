38d7d3b0fb8c8c0b264b205eb576494dccaec087
ronyderra <derrarony@gmail.com>
2024-01-09 10:20:59 +0400

    QueryParameters

--- src/components/queryParameters/queryParameters.jsx
+++ src/components/queryParameters/queryParameters.jsx
@@ -19,9 +19,8 @@ import { ComplexSelect } from "../../origins/select/select";
 
 export const QueryParameters = (props) => {
   const platforms = [
-    { code: JSON.stringify({ query: bing_query, map: bing_query_map }), name: "Bing" },
     { code: JSON.stringify({ query: fb_query, map: fb_query_map }), name: "Facebook" },
-    { code: JSON.stringify({ query: bingLeo_query, map: bingLeo_query_map }), name: "Bing(LEO)" },
+    { code: JSON.stringify({ query: bingLeo_query, map: bingLeo_query_map }), name: "Bing" },
     { code: JSON.stringify({ query: taboola_query, map: taboola_query_map }), name: "Taboola" },
     { code: JSON.stringify({ query: twitter_query, map: twitter_query_map }), name: "Twitter" },
     { code: JSON.stringify({ query: google_query, map: google_query_map }), name: "Google" },
